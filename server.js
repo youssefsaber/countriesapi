984582023a5150fabd5ee1d25cff71d6e554a7d1
youssef.s <youssef.s@ideasoft.io>
2019-07-19 13:48:48 +0300

    minor refactor

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const compression = require('compression');
 
 const errorHandler = require('./helpers/errorHandler');
 const countries = require('./controllers/api.controller');
@@ -11,18 +10,21 @@ const countries = require('./controllers/api.controller');
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(compression());
 app.use(cors());
 
-app.use(express.static('public'));
-// app.use('/static', express.static(path.join(__dirname, 'public')));
-
 // routes
-// app.get('/', (req, res, next) => res.json({ status: 'OK', code: 200 }));
+app.get('/', (req, res, next) => res.json({ status: 'OK', code: 200 }));
 app.use('/countries', countries);
 
 // Question 4 , Slots machine
 app.use('/slots', require('./controllers/slots.controller'));
+
+app.get('*', (req, res, next) =>
+  res.json({
+    status: 404,
+    message: 'route not found'
+  })
+);
 
 //global error handler
 app.use(errorHandler);
